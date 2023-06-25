e724c4d42aa8fe1d3dda84d6e84eddfad084794c
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-06-26 04:24:35 +0900

    アメーバ仮作成

--- html/wp-content/themes/portfolio/front-page.php
+++ html/wp-content/themes/portfolio/front-page.php
@@ -1,10 +1,22 @@
 <?php get_header(); ?>
 <main class="p-front__main">
   <div class="p-frontWrap">
-    <div class="mv">
+    <section class="p-mv">
+      <div class="p-mvWrap">
+        <picture class="p-mv__imgWrap">
+          <source type="image/avif" srcset="<?php echo esc_url(get_template_directory_uri()); ?>/dist/images/only/mv1.avif">
+          <source type="image/webp" srcset="<?php echo esc_url(get_template_directory_uri()); ?>/dist/images/only/mv1.webp">
+          <img src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/images/only/mv1.png" alt="mv1">
+        </picture>
+        <div class="r">
+          <h1 class="p-mv__title">
+            SHUNYA SUGAWARA’s<br>
+            profile website.
+          </h1>
+        </div>
 
-    </div>
-
+      </div>
+    </section>
   </div>
 </main>
 
