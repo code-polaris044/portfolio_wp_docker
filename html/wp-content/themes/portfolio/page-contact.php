78dc613aaa1bddb63cb8b479a39d98005af16670
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-06-28 01:42:34 +0900

    お問い合わせ一旦完了

--- html/wp-content/themes/portfolio/page-contact.php
+++ html/wp-content/themes/portfolio/page-contact.php
@@ -1,8 +1,16 @@
 <?php get_header(); ?>
 
 <main class="aboutMain">
+    <!-- <section class="s_underlayer_common">
+        <?php
+        include('underlayer_mv/template02.php');
+        ?>
+    </section> -->
+    <section class="s_contact">
+        <?php echo do_shortcode('[mwform_formkey key="50"]'); ?>
+    </section>
 
-
+    <!-- ローカルは22 -->
 
 </main>
 <?php get_footer(); ?>
