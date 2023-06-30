90d29694ea2f960aa381b53a7644fa4472d828ff
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-07-01 07:30:05 +0900

    カスタム投稿タイプの表示数変更

--- html/wp-content/themes/portfolio/functions.php
+++ html/wp-content/themes/portfolio/functions.php
@@ -290,7 +290,7 @@ function change_posts_per_page($query)
 	if (is_admin() || !$query->is_main_query())
 		return;
 	if ($query->is_post_type_archive('gallery')) { //カスタム投稿タイプを指定
-		$query->set('posts_per_page', -1); //表示件数を指定
+		$query->set('posts_per_page', 9); //表示件数を指定
 	}
 }
 add_action('pre_get_posts', 'change_posts_per_page');
