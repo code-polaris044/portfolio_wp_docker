fa9aea6d7ad78d636f691d14e6e272249492e3f8
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-07-01 08:51:24 +0900

    sitemap add

---
+++ html/wp-content/themes/portfolio/page-sitemap.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<section class="p-contact">
+  <div class="p-contactWrap">
+    <?php echo do_shortcode('[wp_sitemap_page] '); ?>
+  </div>
+</section>
+
+</section>
+
+<?php get_footer(); ?>
