526e029c90dd0f2baf5fcecc99faec1c1dedbf85
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-07-01 21:33:58 +0900

    insta.php add

--- html/wp-content/themes/portfolio/front-page.php
+++ html/wp-content/themes/portfolio/front-page.php
@@ -112,29 +112,35 @@
     </section>
 
 
-  </div>
-  <section class="p-front__contact">
-    <div class="p-front__contactWrap">
-      <div class="p-front__map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23037.818019467726!2d141.3254432754836!3d43.10908003415067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f0b28b9df4066d3%3A0x507d400b452d8353!2z6bq755Sf6aeF!5e0!3m2!1sja!2sjp!4v1687806058381!5m2!1sja!2sjp" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-        </iframe>
-      </div>
-      <div class="p-front__contact__textarea">
-        <h2 class="p-front__section__title u-mb__30">お問い合わせ</h2>
-        <p class="p-front__section__lead u-mb__20">お気軽にお問い合わせください</p>
-        <address class="p-front__address">
-          <h3 class="p-front__section__subtitle u-mb__20">TEL</h3>
-          <p class="p-front__section__text u-mb__20">080-0000-000</p>
-          <h3 class="p-front__section__subtitle u-mb__20">住所</h3>
-          <p class="p-front__section__text u-mb__50">札幌市 北区 新琴似</p>
-        </address>
-        <button class="p-front__btn">
-          <a href="<?php echo get_the_permalink(); ?>/contact/" class="p-front__btn__link">Contact</a>
-        </button>
-      </div>
+    <section class="p-instagram">
 
-    </div>
-  </section>
+    </section>
+
+
+
+    <section class="p-front__contact">
+      <div class="p-front__contactWrap">
+        <div class="p-front__map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23037.818019467726!2d141.3254432754836!3d43.10908003415067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f0b28b9df4066d3%3A0x507d400b452d8353!2z6bq755Sf6aeF!5e0!3m2!1sja!2sjp!4v1687806058381!5m2!1sja!2sjp" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+          </iframe>
+        </div>
+        <div class="p-front__contact__textarea">
+          <h2 class="p-front__section__title u-mb__30">お問い合わせ</h2>
+          <p class="p-front__section__lead u-mb__20">お気軽にお問い合わせください</p>
+          <address class="p-front__address">
+            <h3 class="p-front__section__subtitle u-mb__20">TEL</h3>
+            <p class="p-front__section__text u-mb__20">080-0000-000</p>
+            <h3 class="p-front__section__subtitle u-mb__20">住所</h3>
+            <p class="p-front__section__text u-mb__50">札幌市 北区 新琴似</p>
+          </address>
+          <button class="p-front__btn">
+            <a href="<?php echo get_the_permalink(); ?>/contact/" class="p-front__btn__link">Contact</a>
+          </button>
+        </div>
+
+      </div>
+    </section>
   </div>
-</main><?php get_footer();
-        ?>
+</main>
+
+<?php get_footer(); ?>
